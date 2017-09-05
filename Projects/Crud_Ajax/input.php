aa13db8168f84e13b06554998b517488357e44b7
Naga Sai Manoj Giddaluru <nagasai@Nagas-MacBook-Pro.local>
2017-09-05 07:24:03 +0530

    organised a bit

---
+++ Projects/Crud_Ajax/input.php
@@ -0,0 +1,44 @@
+<html>
+<body>
+<table class='w3-table-all w3-card-4 w3-small '>
+    <tr class='w3-red'>
+        <td colspan='4'>
+            <h3 align='center'>Add new Records</h3>
+        </td>
+    </tr>
+    <tr class='w3-cyan'>
+        <th>Name</th>
+        <th>Age</th>
+        <th>Phone</th>
+        <th></th>
+    </tr>
+    <tr>
+        <td>
+            <input style='width:100%;' type='text' class='textinput' name='uname' onkeydown='if (event.keyCode === 27)
+                                reset();' placeholder='your name' id='uname' required='yes' autofocus='yes'/>
+        </td>
+        <td>
+            <input style='width:100%;' type='text' class='textinput' name='uage' onkeydown='if (event.keyCode === 27)
+                                reset();' placeholder='your age' id='uage' required='yes'/>
+        </td>
+        <td>
+            <input style='width:100%;' type='text' class='textinput' name='uphone' onkeydown='
+                            switch (event.keyCode) {
+                                case 13:
+                                    push();
+                                    break;
+                                case 27:
+                                    reset();
+                                    break;
+                            }
+                           ' placeholder='your phone number' id='uphone' required='yes'/>
+        </td>
+        <td>
+            <button class='w3-button w3-hover-green' name='submit' style='width:100%; color:green' onclick='push()'>Add
+                Record
+            </button>
+        </td>
+    </tr>
+</table>
+</body>
+</html>
---
+++ Projects/Crud_noAjax/editrow.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Person Details</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+</head>
+<body>
+<?php
+$conn = mysqli_connect('localhost', 'admin', 'password', 'testphp') or die("Connection failed: " . mysqli_connect_error());
+$query = "select * from users where serial =" . $_REQUEST["rowNumber"];
+
+$result = mysqli_query($conn, $query);
+
+if (mysqli_num_rows($result) > 0) {
+    echo "<h3 align='center'>Row Updation</h3>
+		<div class='w3-container w3-responsive'>
+		<form action='db.php' method='post'>
+        <input type='hidden' name='formname' value='update'/>
+		<table class='w3-table-all w3-card-4 w3-small'>
+		<tr class='w3-red'>
+		<th>Serial</th>
+		<th>Name</th>
+		<th>Age</th>
+		<th>Phone</th>
+		<th></th>
+		</tr>";
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "<tr>
+			<td><input type='text' style='width:100%' name='serial' value='" . $row['serial'] . "' readonly/></td>
+			<td><input type='text' style='width:100%;' name='name' value='" . $row['name'] . "'/></td>
+			<td><input type='text' style='width:100%;' name='age' value='" . $row['age'] . "'/></td>
+			<td><input type='text' style='width:100%;' name='phone' value='" . $row['phone'] . "'/></td>
+			<td><input type='submit' style='width:100%;' value='update'></td></tr>";
+    }
+    echo "</table></form></div>";
+} else {
+    echo "<h3 align='center'>No Data in existing Records</h3>";
+}
+mysqli_close($conn);
