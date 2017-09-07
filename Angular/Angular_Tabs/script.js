f88eb965a811b346b2a40145c1e5fec9d7e98c1b
Naga Sai Manoj <nagasai.g9@gmail.com>
2017-09-07 19:39:47 +0530

    employee tabbed

---
+++ Angular/Angular_Tabs/script.js
@@ -0,0 +1,13 @@
+var app = angular.module("myApp", ["ngSanitize"]);
+
+app.controller("myCtrl", function($scope) {
+  $scope.homebtn = function() {
+    $scope.target = "<h1>this is about_page";
+  };
+  $scope.addbtn = function() {
+    $scope.target = "<h1>this is to add nwe employee";
+  };
+  $scope.showlist = function() {
+    $scope.target = "<h1>this is to display existing employees";
+  };
+});
