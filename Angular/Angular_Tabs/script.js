10e698673cd157d5ce2404a3e4e38260a82cb0a7
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2017-09-09 16:14:55 +0530

    slight changes

--- Angular/Angular_Tabs/script.js
+++ Angular/Angular_Tabs/script.js
@@ -1,13 +1,13 @@
 var app = angular.module("myApp", ["ngSanitize"]);
 
-app.controller("myCtrl", function($scope) {
-  $scope.homebtn = function() {
+app.controller("myCtrl", function ($scope) {
+  $scope.homebtn = function () {
     $scope.target = "<h1>this is about_page";
   };
-  $scope.addbtn = function() {
+  $scope.addbtn = function () {
     $scope.target = "<h1>this is to add nwe employee";
   };
-  $scope.showlist = function() {
+  $scope.showlist = function () {
     $scope.target = "<h1>this is to display existing employees";
   };
 });
