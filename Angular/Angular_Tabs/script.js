240b4a89244fe9f7d6169db72eb9e4cc42db61b3
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2017-09-09 16:16:48 +0530

    accepted incomong changes

--- Angular/Angular_Tabs/script.js
+++ Angular/Angular_Tabs/script.js
@@ -1,16 +1,5 @@
 var app = angular.module("myApp", []);
 
-<<<<<<< HEAD
-app.controller("myCtrl", function ($scope) {
-  $scope.homebtn = function () {
-    $scope.target = "<h1>this is about_page";
-  };
-  $scope.addbtn = function () {
-    $scope.target = "<h1>this is to add nwe employee";
-  };
-  $scope.showlist = function () {
-    $scope.target = "<h1>this is to display existing employees";
-=======
 app.controller("myCtrl", function($scope, $http) {
   $scope.homebtn = function() {
     $http.get("about.txt").then(function(response) {
@@ -26,6 +15,5 @@ app.controller("myCtrl", function($scope, $http) {
     $http.get("emplist.txt").then(function(response) {
       $scope.target = response.data;
     });
->>>>>>> 19ed038814587896b6aba3ed489fb054c8c96a45
   };
 });
